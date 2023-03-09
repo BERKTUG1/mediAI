0306de17aaabdae35eb562c0d10707df667a4654
Berktug <berktug2911@gmail.com>
2023-03-09 16:48:50 +0000

    Fixed application

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 from flask import Flask
 
-app = Flask(__name__)
+application = Flask(__name__)
 
-@app.route("/")
+@application.route("/")
 def hello_world():
     return "<p>Roberto ist ein G</p>"
 
 if __name__ == '__main__':
-    app.run()
+    application.run()
